0c6bba7af11b17cde5f5ab80cc8cef0b50b08f08
Hunter Monroe <huntis@outlook.com>
2022-10-31 10:44:55 -0600

    Create form

---
+++ src/components/Form/Form.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    constructor() {
+        super() 
+        this.state = {
+            name: '', 
+            date: '',
+            time: ' ', 
+            number: ''
+        }
+    }
+
+    handleChange = (event) => {
+        const {name , value } = event.target
+        this.setState({[name]: [value]})
+    }
+
+
+
+
+
+
+  render() {
+    return (
+      <div className='form-container'>
+        <form>
+            <input 
+            type = 'text'
+            placeholder='name'
+            name= "name"
+            value={this.state.name}
+            onChange = {event => this.handleChange(event)}
+            /> 
+            <input 
+            type ='date(mm/dd)'
+            placeholder='date'
+            name = 'date'
+            value = {this.state.date}
+            onChange = {event => this.handleChange(event)}
+            /> 
+            <input 
+            type = 'time'
+            placeholder='time'
+            name ='time'
+            value={this.state.time}
+            onChange = {event => this.handleChange(event)}
+            /> 
+            <input 
+            type = 'number'
+            placeholder='number of people'
+            name = 'number'
+            value = {this.state.time}
+            onChange = {event => this.handleChange(event)}
+            /> 
+
+            <button> Make Reservations</button> 
+
+
+        </form>
+        
+      </div>
+    )
+  }
+}
+
+export default Form
